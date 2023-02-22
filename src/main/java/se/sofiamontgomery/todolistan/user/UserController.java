4d11f163e02c41c7f85ec2e7bb89651e8c43cf85
Sofia <sofia.montgomery@gmail.com>
2023-02-22 21:45:24 +0100

    added list of users, removed buttons

--- src/main/java/se/sofiamontgomery/todolistan/user/UserController.java
+++ src/main/java/se/sofiamontgomery/todolistan/user/UserController.java
@@ -2,12 +2,19 @@ package se.sofiamontgomery.todolistan.user;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 import se.sofiamontgomery.todolistan.auth.AppRoles;
 import se.sofiamontgomery.todolistan.config.AppPasswordConfig;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -25,6 +32,24 @@ public class UserController {
     public String displayRegisterUser(UserModel userModel) {
         return "register";
     }
+
+    //PRIO:
+    //gör så den visar allt i todolistan
+    //gör så man kan lägga till i sin lista
+    //gör så man kan ta bort från sin lista
+
+    //OM JAG FÅR TID:
+    //gör så den visar *specifik användare* på userhome och admin
+    //gör så man kan ta bort en användare på admin
+
+    @GetMapping("/admin")
+    public ModelAndView displayUserHome(UserModel userModel) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("admin");
+        modelAndView.addObject("user", userRepository.findAll());
+        return modelAndView;
+    }
+
 
     @PostMapping("/register")
     public String registerUser(@Valid UserModel userModel, BindingResult result) {
