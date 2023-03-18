9ff1eb4183e25a1b8e914417be1534be095d9d09
Sofia <sofia.montgomery@gmail.com>
2023-03-18 21:38:32 +0100

    removed unused imports

--- src/main/java/se/sofiamontgomery/todolistan/user/UserController.java
+++ src/main/java/se/sofiamontgomery/todolistan/user/UserController.java
@@ -2,12 +2,9 @@ package se.sofiamontgomery.todolistan.user;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import se.sofiamontgomery.todolistan.auth.AppRoles;
