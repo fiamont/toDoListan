98584627170658cce9e0fad41930d40fd18b1c08
Sofia <sofia.montgomery@gmail.com>
2023-03-19 12:42:25 +0100

    moved methods from controller to service

--- src/main/java/se/sofiamontgomery/todolistan/user/UserController.java
+++ src/main/java/se/sofiamontgomery/todolistan/user/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import se.sofiamontgomery.todolistan.auth.AppRoles;
 import se.sofiamontgomery.todolistan.config.AppPasswordConfig;
 
 @Controller
@@ -15,11 +14,13 @@ public class UserController {
 
     private final UserRepository userRepository;
     private final AppPasswordConfig appPasswordConfig;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserRepository userRepository, AppPasswordConfig appPasswordConfig) {
+    public UserController(UserRepository userRepository, AppPasswordConfig appPasswordConfig, UserService userService) {
         this.userRepository = userRepository;
         this.appPasswordConfig = appPasswordConfig;
+        this.userService = userService;
     }
 
     @GetMapping("/register")
@@ -29,32 +30,12 @@ public class UserController {
 
     @PostMapping("/register")
     public String registerUser(@Valid UserModel userModel, BindingResult result) {
-        if(result.hasErrors()){
-            return "register";
-        }
-        String role = String.valueOf(userModel.getAuthorities().iterator().next());
-
-        switch (role) {
-            case "Admin" -> userModel.setAuthorities(AppRoles.ADMIN.getGrantedAuthorities());
-            case "User" -> userModel.setAuthorities(AppRoles.USER.getGrantedAuthorities());
-        }
-
-        userModel.setPassword(appPasswordConfig.bcryptPasswordEncoder().encode(userModel.getPassword()));
-        userModel.setAccountNonExpired(true);
-        userModel.setAccountNonLocked(true);
-        userModel.setCredentialsNonExpired(true);
-        userModel.setEnabled(true);
-
-        userRepository.save(userModel);
-        return "index";
+        return userService.createUser(userModel, result);
     }
 
     @GetMapping("/admin")
     public ModelAndView displayAllUsers(UserModel userModel) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("admin");
-        modelAndView.addObject("users", userRepository.orderByUsername());
-        return modelAndView;
+        return userService.showAllUsers(userModel);
     }
 
     //TODO: OM JAG FÅR TID/KAN KLURA UT:
--- src/main/java/se/sofiamontgomery/todolistan/user/UserService.java
+++ src/main/java/se/sofiamontgomery/todolistan/user/UserService.java
@@ -1,23 +1,59 @@
 package se.sofiamontgomery.todolistan.user;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
+import se.sofiamontgomery.todolistan.auth.AppRoles;
+import se.sofiamontgomery.todolistan.config.AppPasswordConfig;
 
 @Service
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    private final AppPasswordConfig appPasswordConfig;
+
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, AppPasswordConfig appPasswordConfig) {
         this.userRepository = userRepository;
+        this.appPasswordConfig = appPasswordConfig;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
+    }
+
+    public String createUser(@Valid UserModel userModel, BindingResult result) {
+        if(result.hasErrors()){
+            return "register";
+        }
+        String role = String.valueOf(userModel.getAuthorities().iterator().next());
+
+        switch (role) {
+            case "Admin" -> userModel.setAuthorities(AppRoles.ADMIN.getGrantedAuthorities());
+            case "User" -> userModel.setAuthorities(AppRoles.USER.getGrantedAuthorities());
+        }
+
+        userModel.setPassword(appPasswordConfig.bcryptPasswordEncoder().encode(userModel.getPassword()));
+        userModel.setAccountNonExpired(true);
+        userModel.setAccountNonLocked(true);
+        userModel.setCredentialsNonExpired(true);
+        userModel.setEnabled(true);
+
+        userRepository.save(userModel);
+        return "index";
+    }
+
+    public ModelAndView showAllUsers(UserModel userModel) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("admin");
+        modelAndView.addObject("users", userRepository.orderByUsername());
+        return modelAndView;
     }
 }
