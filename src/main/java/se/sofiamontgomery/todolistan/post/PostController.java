4159d8f50c7eae035099e65f08e6c8bf7177ce73
Sofia <sofia.montgomery@gmail.com>
2023-03-23 21:46:55 +0100

    removed comments, add postlist to table in userhome

--- src/main/java/se/sofiamontgomery/todolistan/post/PostController.java
+++ src/main/java/se/sofiamontgomery/todolistan/post/PostController.java
@@ -19,15 +19,6 @@ public class PostController {
         this.postRepository = postRepository;
     }
 
-    /*TODO:
-       gör så den visar allt tillhörande endast den inloggade i todolistan
-       gör så man kan lägga till i sin lista
-       gör så man kan ta bort från sin lista
-       gör så man kan ändra sin lista
-       om jag har tid:
-       gör så när det är färdigt så stryks det över istället för tas bort helt och hållet
-     */
-
     @GetMapping("/userhome")
     public ModelAndView getListItems(PostModel postModel, UserModel userModel) {
         ModelAndView modelAndView = new ModelAndView();
@@ -35,7 +26,6 @@ public class PostController {
         modelAndView.addObject("post", postRepository.findAll());
         return modelAndView;
     }
-
 
     @PostMapping("/userhome")
     public String addListItem(@Valid PostModel postModel, BindingResult result, UserModel userModel) {
--- src/main/java/se/sofiamontgomery/todolistan/post/PostRepository.java
+++ src/main/java/se/sofiamontgomery/todolistan/post/PostRepository.java
@@ -2,10 +2,12 @@ package se.sofiamontgomery.todolistan.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import se.sofiamontgomery.todolistan.user.UserModel;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostModel, Long> {
 
-    //T.ex. @Query("SELECT s FROM PostModel s WHERE s.userModel.userid=5")
-    //List<PostModel> findByUserid(UserModel user);
+    //List<PostModel> findAllByUserModel(UserModel userModel);
 }
